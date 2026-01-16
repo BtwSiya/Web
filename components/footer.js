575fa4abbf5056ff164be8f2b80bccaeccb3af36
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-01-17 00:20:17 +0530

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <a href="/privacy">Privacy</a> | 
+      <a href="/terms">Terms</a> | 
+      <a href="/refund">Refund</a> | 
+      <a href="/shipping">Shipping</a> | 
+      <a href="/contact">Contact</a>
+    </footer>
+  );
+}
