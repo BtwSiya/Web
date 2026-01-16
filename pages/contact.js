50f23caadea11ec057fb6b835d60200074616b01
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-01-17 00:24:06 +0530

    Create contact.js

---
+++ pages/contact.js
@@ -0,0 +1,10 @@
+export default function Contact() {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Email: teamtoxiclabs@gmail.com</p>
+      <p>Phone: 9509203839</p>
+      <p>Brand: ToxicLabs</p>
+    </div>
+  );
+}
