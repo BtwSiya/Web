95064e4970591296b8f4b7e687618432eba4005b
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-01-17 00:22:36 +0530

    Create terms.js

---
+++ pages/terms.js
@@ -0,0 +1,15 @@
+export default function Terms() {
+  return (
+    <div className="container">
+      <h1>Terms & Conditions</h1>
+      <p>
+        Welcome to ToxicLabs. By accessing our website, you agree to comply
+        with our terms. All products are for personal use only.
+      </p>
+      <p>
+        We reserve the right to update prices, policies, or content at any time
+        without prior notice.
+      </p>
+    </div>
+  );
+}
