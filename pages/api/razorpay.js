4c3a1c25c6b1ddfd0aaebe67a15c7411e0220fa2
𝘚𝘪𝘺𝘢 ‹𝟹 <siyatechbot@gmail.com>
2026-01-17 00:18:35 +0530

    Create razorpay.js

---
+++ pages/api/razorpay.js
@@ -0,0 +1,15 @@
+import Razorpay from "razorpay";
+
+export default async function handler(req, res) {
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+
+  const order = await razorpay.orders.create({
+    amount: 150000,
+    currency: "INR"
+  });
+
+  res.status(200).json(order);
+}
